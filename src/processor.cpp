a8d5132e902db9fad0a5ae5674b08bcb88ef3f58
AchintyaRaja <87812346+AchintyaRaja@users.noreply.github.com>
2021-10-04 19:48:00 +1100

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -3,12 +3,16 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <chrono>
+#include <thread>
+using namespace std::this_thread;
+using namespace std::chrono_literals;
 
 using std::string;
 using std::ifstream;
 using std::istringstream;
 using std::stof;
-
+using std::chrono::system_clock;
 // TODO: Return the aggregate CPU utilization
 /*
 PrevIdle = previdle + previowait
@@ -25,12 +29,12 @@ totald = Total - PrevTotal
 idled = Idle - PrevIdle
 
 CPU_Percentage = (totald - idled)/totald
-stack overflow post: https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
 */
 float Processor::Utilization() {
   string key;
   string line;
   string use, nic, idl, syste, iowai, ir, softIr, stea;
+  string fuse, fnic, fidl, fsyste, fiowai, fir, fsoftIr, fstea;
   string file = "/proc/stat";
   ifstream cpustream(file);
   if(cpustream.is_open()){
@@ -47,11 +51,9 @@ float Processor::Utilization() {
           irq = stof(ir);
           softIrq = stof(softIr);
           steal = stof(stea);
-          float totIdle = iowait + idle;
-          float totAct = user + nice + system + irq + softIrq + steal;
-          float tot = totIdle + totAct;
-          float aggCPU = (tot-totIdle)/tot;
-          return aggCPU;
+          float idld = idle + iowait;
+          float totd = user + nice + idle + system + iowait + irq + softIrq + steal;
+          return (totd-idld)/totd; //I believe I am doing everything to get the correct CPU utilisation, and yet it comes out wrong
         }
       }
     }
